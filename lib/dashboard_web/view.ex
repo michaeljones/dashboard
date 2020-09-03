29c3b161b67baf6442dc2b1614b525f8783a9627
Michael Jones <m.pricejones@gmail.com>
2020-09-03 13:36:09 +0100

    Further display adjustments
    Mostly involving wresting with getting the list of commits on to one
    line and being well behaved. I've had to add the 'width: 80vw' in order
    to keep the width under control. There must be a better way to handle
    this but I've not been able to figure out it.
    
    We also individually color the links so that we can make the author
    links a different colour. Not sure of this is better but it at least it
    introduces a little visible variety.
    
    Also remove the grey background from the repo name on the 'new commits'
    lines in order to try to get them to fade into the background a little
    more.

--- lib/dashboard_web/view.ex
+++ lib/dashboard_web/view.ex
@@ -4,41 +4,59 @@ defmodule DashboardWeb.View do
   alias Phoenix.HTML.Link
 
   def actor_link(actor) do
-    Link.link(actor["login"], to: "https://github.com/#{actor["login"]}")
+    Link.link(actor["login"], to: "https://github.com/#{actor["login"]}", class: "text-secondary")
   end
 
-  def repo_link(repo) do
+  def repo_link(repo, opts \\ []) do
+    quiet = Keyword.get(opts, :quiet, false)
+
+    class =
+      if quiet do
+        "bg-white border border-grey-6"
+      else
+        "bg-grey-6"
+      end
+
     Link.link(repo["name"],
       to: "https://github.com/#{repo["name"]}",
-      class: "bg-grey-6 px-2 py-1 rounded"
+      class: class <> " px-2 py-1 rounded text-mid-blue"
     )
   end
 
   def release_link(release) do
-    Link.link(release["tag_name"], to: "#{release["html_url"]}")
+    Link.link(release["tag_name"], to: "#{release["html_url"]}", class: "text-mid-blue")
   end
 
   def issue_link(issue) do
-    Link.link("#" <> Integer.to_string(issue["number"]), to: issue["html_url"])
+    Link.link("#" <> Integer.to_string(issue["number"]),
+      to: issue["html_url"],
+      class: "text-mid-blue"
+    )
   end
 
   def pr_title_link(pr) do
-    Link.link(pr["title"], to: pr["html_url"])
+    Link.link(pr["title"], to: pr["html_url"], class: "text-mid-blue")
   end
 
   def commit_link(commit, repo) do
-    Link.link(commit["message"], to: "https://github.com/#{repo["name"]}/commit/#{commit["sha"]}")
+    first_line = commit["message"] |> String.split("\n") |> List.first()
+
+    Link.link(first_line,
+      to: "https://github.com/#{repo["name"]}/commit/#{commit["sha"]}",
+      class: "text-mid-blue"
+    )
   end
 
   def sha_link(sha, repo) do
     Link.link(String.slice(sha, 0, 10) <> "...",
-      to: "https://github.com/#{repo["name"]}/commit/#{sha}"
+      to: "https://github.com/#{repo["name"]}/commit/#{sha}",
+      class: "text-mid-blue"
     )
   end
 
   def unhandled(entry) do
     ~E"""
-      <li class="base-entry">
+      <li class="base-entry border-secondary">
         <%= repo_link entry["repo"] %> <%= entry["type"] %> - <%= Timex.from_now(entry["datetime"]) %>
       </li>
     """
